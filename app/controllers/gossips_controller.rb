94e8c2660ba105fa4ca80a96852f6893d2c4c1ee
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-05 19:12:58 +0100

    Redir

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,9 +1,5 @@
 class GossipsController < ApplicationController
 
-	def show
-    @gossip = Gossip.find(params[:id])
-  end
-
 	def new
     # Méthode qui crée un potin vide et l'envoie à une view qui affiche le formulaire pour 'le remplir' (new.html.erb)
     @gossip = Gossip.new
@@ -15,7 +11,7 @@ class GossipsController < ApplicationController
   	# Une fois la création faite, on redirige généralement vers la méthode show (pour afficher le potin créé)
   	@gossip = Gossip.new('user' => User.find(11), 'title' => params[:title], 'content' => params[:content])
   	if @gossip.save #ici on essaye de sauvegarder en base @gossip
-  		redirect_to gossips_path #si OK, redirige vers la page index
+  		redirect_to welcome_path #si OK, redirige vers la page index
   	else
   		render 'new' # sinon, il render la view new (celle sur laquelle on est déja)
   	end
