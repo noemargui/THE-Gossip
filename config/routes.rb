5e72370842cd831ef8376fcdb4286027d36963fc
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-05 11:42:30 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/static_pages/welcome', to: 'static_pages#welcome'
-  get '/static_pages/welcome/:id', to: 'static_pages#welcome'
-  get '/static_pages/team', to: 'static_pages#team'
-  get '/static_pages/contact', to: 'static_pages#contact'
-  get '/static_pages/gossipdetails/:id', to: 'static_pages#gossipdetails'
-  get '/static_pages/authordetails/:id', to: 'static_pages#authordetails'
+  get 'https://the-one-and-only-gossip.herokuapp.com/welcome', to: 'static_pages#welcome'
+  get 'https://the-one-and-only-gossip.herokuapp.com/welcome/:id', to: 'static_pages#welcome'
+  get 'https://the-one-and-only-gossip.herokuapp.com/team', to: 'static_pages#team'
+  get 'https://the-one-and-only-gossip.herokuapp.com/contact', to: 'static_pages#contact'
+  get 'https://the-one-and-only-gossip.herokuapp.com/gossipdetails/:id', to: 'static_pages#gossipdetails'
+  get 'https://the-one-and-only-gossip.herokuapp.com/authordetails/:id', to: 'static_pages#authordetails'
 end
