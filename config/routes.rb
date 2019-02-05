143d2f71b64434db37771681ae2dfd48d5d2cc9a
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-05 15:18:33 +0100

    Route change

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/static_pages/welcome', to: 'static_pages#welcome'
-  get '/static_pages/welcome/:id', to: 'static_pages#welcome'
-  get '/static_pages/team', to: 'static_pages#team'
-  get '/static_pages/contact', to: 'static_pages#contact'
-  get '/static_pages/gossipdetails/:id', to: 'static_pages#gossipdetails'
-  get '/static_pages/authordetails/:id', to: 'static_pages#authordetails'
+  get '/welcome', to: 'static_pages#welcome'
+  get '/welcome/:id', to: 'static_pages#welcome'
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
+  get '/gossipdetails/:id', to: 'static_pages#gossipdetails'
+  get '/authordetails/:id', to: 'static_pages#authordetails'
 end
